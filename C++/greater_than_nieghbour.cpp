754d8527bf5deb679356f7f299deeee30135924c
mittalji567 <amitmittal20001015@gmail.com>
2020-04-20 11:39:03 +0530

    jk

---
+++ C++/greater_than_nieghbour.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for(t;t>0;t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(n;n>0;n--){
+            cin>>n;
+        }
+        if(arr[0]>arr[1])
+            
+    }
+}
