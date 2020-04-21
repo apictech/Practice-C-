b9e83e265185881fae98beefec615a5d35d38118
mittalji567 <amitmittal20001015@gmail.com>
2020-04-21 11:05:00 +0530

    Greter than nieghbor

--- C++/greater_than_nieghbour.cpp
+++ C++/greater_than_nieghbour.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int checkbig(int arr[], int n){
+    if(arr[0]>arr[1])
+        cout<<0;
+    if(arr[n-1]>arr[n-2]){
+        cout<<n-1;
+    }
+    for(int i=1;i<n-1;i++){
+        if(arr[i-1]>arr[i] && arr[i+1]>arr[i])
+            cout<<i;
+        else
+            continue;
+    }
+    return 0;
+}
+
 int main(){
     int t;
     cin>>t;
@@ -11,7 +26,7 @@ int main(){
         for(n;n>0;n--){
             cin>>n;
         }
-        if(arr[0]>arr[1])
-            
+        checkbig(arr,n);
     }
+    return 0;
 }
