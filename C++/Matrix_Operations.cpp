c943d9c0f386700cb9ab0bf3fd02796ceca58d1a
mittalji567 <amitmittal20001015@gmail.com>
2020-05-10 19:33:43 +0530

    Matrix-program-started

---
+++ C++/Matrix_Operations.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int m, n;
+    cin>>m>>n;
+    int arr1[m][n], arr2[m][n], arr3[m][n];
+
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr1[i][j];
+        }
+    }
+
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr2[i][j];
+        }
+    }
+
+    
+
+    return 0;
+}
