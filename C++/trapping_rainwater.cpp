48a80e23c0a139102babed267a738997f7717cf8
Amit Mittal <amitmittal20001015@gmail.com>
2020-10-03 15:47:21 +0530

    trapping_rainwater_bruteforce_added

--- C++/trapping_rainwater.cpp
+++ C++/trapping_rainwater.cpp
@@ -2,6 +2,7 @@
 This is the famous Trapping Rain Water problem which is asked in many interviews.
 */
 
+// Brute-force Approach.
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -15,46 +16,21 @@ int main(){
     }
 
     int water = 0;
-    int left_max = arr[0], right_max = arr[n-1];
 
-    for(int i=1; i<n-1; i++){
-        for(int j=i; j>=0; j--){
-            if(arr[j] > left_max)
-                left_max = arr[j];
-            cout<<left_max;
+    for(int i=0; i < n ; i++){
+        int left_max = 0, right_max=0;
+        
+        for(int j = i; j > 0; j--){
+            left_max = max(left_max, arr[j]);
         }
-        for(int j=i; j<n; j++){
-            if(arr[j] > right_max)
-                right_max = arr[j];
-            // cout<<right_max;
+
+        for(int j = i; j < n; j++){
+            right_max = max(right_max, arr[j]);
         }
 
         water += min(left_max, right_max) - arr[i];
     }
 
-    // cout<<water;
+    cout<<water;
+
 }
-
-    // int left[n], right[n], water = 0;
-    // left[0] = arr[0];
-    // for(int i=1; i<n; i++){
-    //     if(arr[i]>left[i-1])
-    //         left[i] = arr[i];
-    //     else
-    //         left[i] = left[i-1];
-    // }
-
-    // for(int i=n-1; i>=0; i--){
-    //     if(arr[i]>right[i+1])
-    //         right[i] = arr[i];
-    //     else
-    //         right[i] = right[i+1];
-    // }
-
-    // for(int i=1; i<n-1; i++){
-    //     water += min(left[i],right[i]) - arr[i];
-    // }
-
-    // cout<<water;
-// }
-
