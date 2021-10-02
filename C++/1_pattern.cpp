8953f1bc911b0b36937204cd195009a8f551812e
Akash giri <akashgiri1020@gmail.com>
2021-10-02 12:19:20 +0530

    Update 1_pattern.cpp
    plz specify each  line with the comment it is mandatory for a programmer

--- C++/1_pattern.cpp
+++ C++/1_pattern.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
+    // variable declared..
     int i,k,n;
+    // take input n ..
     cin>>n;
+    // use loop for counting ...
     for(i=1;i<=5;i++){
         cout<<" ";
         for(k=1;k<=i;k++){
